2a711a8a859ded9b6b0b5ec107f98a873f0f6eaf
Stéphane Marcilus <stephane.marcilus@gmail.com>
2020-09-28 16:31:54 +0200

    Activation des modèles

--- models.py
+++ models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Question(models.Model): #L'objet Question (d'un sondage) contient une question et une date de publication
+    question_text = models.CharField(max_length=200) #CharField -> champs de caractères
+    pub_date = models.DateTimeField('date published') #DateTimeField -> champs date et heure
 
 
-class Choice(models.Model):
+class Choice(models.Model): #L'objet Choice contient un texte représentant le choix et le décompte des votes
+                            #Chaque choix est associé à une Question
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    #1. Créer ou modifier les modèles ici
+    #2. Commande 'py manage.py makemigrations' pour créer des migrations associées aux modèles
+    #3. Commande 'py manage.py migrate' pour appliquer les créations/modifications à la base de données
